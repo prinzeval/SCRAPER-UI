6a45e8089343744889f7f35beed128ce0cb4121d
prinzeval <valentineallpowers@gmail.com>
2024-08-21 23:44:50 +0300

    1

--- scraper-ui/vite.config.ts
+++ scraper-ui/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000', // Adjust as needed
+      '/api': 'https://backend-f7q7.onrender.com/', // Adjust as needed
     },
   },
 });
