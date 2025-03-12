ed8a064b3d03b12155d9c76554acacb29cb31c8a
prinzeval <valentineallpowers@gmail.com>
2025-03-12 03:10:00 +0200

    ok

--- scraper-ui/src/api.tsx
+++ scraper-ui/src/api.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Set the base URL for the API requests
 const api = axios.create({
-  baseURL: "https://backend-f7q7.onrender.com/", // Replace with your FastAPI backend URL
+  baseURL: "http://localhost:8000/", // Replace with your FastAPI backend URL
   headers: {
     "Content-Type": "application/json", // Ensure content type is set
   },
--- scraper-ui/vite.config.ts
+++ scraper-ui/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://backend-f7q7.onrender.com/', // Adjust as needed
+      '/api': 'http://localhost:8000', // Adjust as needed https://backend-f7q7.onrender.com/
     },
   },
 });
